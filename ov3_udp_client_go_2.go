dba050c5577b70d697f6bb6f0db67f22a589ff2a
Lars-Christian Tokle <lars-christian.tkole@hotmail.com>
2016-01-28 16:51:29 +0100

    Fjernet litt kode og laget en annen port slik at to PCer kan kommunisere sammen.

---
+++ ov3_udp_client_go_2.go
@@ -0,0 +1,65 @@
+// Go 1.2
+// go run helloworld_go.go
+
+package main
+ 
+import (
+    "net"
+    "time"
+    "log"
+    // "fmt"
+)
+
+func ListenThing(conn *net.UDPConn, read_channel chan []byte) {
+    for {
+        data := make([]byte, 1024)
+        log.Println("Attempt read")
+        _, _, err := conn.ReadFromUDP(data)
+        if err != nil {
+            log.Fatal(err)
+        }
+        read_channel <- data
+
+    }
+}
+
+func WriteThing(conn *net.UDPConn) {
+    for {
+        data := []byte("Testing 123")
+        sent_bytes, err := conn.Write(data)
+        if err != nil {
+            log.Fatal(err)
+        }
+        log.Println("Sent", sent_bytes, "bytes")
+        time.Sleep(1 * time.Second)
+    }
+}
+
+func main() {
+    // Listen for data through this address:port
+    //local, err :=  net.ResolveUDPAddr("udp4", ":20010")
+    //conn2, err := net.ListenUDP("udp4", local)
+    //if err != nil {
+    //    log.Fatal(err)
+    //}
+
+    // Send data to this address:port (broadcast address)
+    remote, err := net.ResolveUDPAddr("udp4","129.241.187.149:29999")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    conn, err := net.DialUDP("udp4", nil, remote)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    read_channel := make(chan []byte)
+
+    //go ListenThing(conn2, read_channel)
+    go WriteThing(conn)
+
+    for {
+        log.Println(string(<- read_channel))
+    }
+}
---
+++ ov3_udp_server_go_2.go
@@ -0,0 +1,43 @@
+// Go 1.2
+// go run helloworld_go.go
+// my ip: 129.241.187.148
+
+package main
+ 
+import (
+    "fmt"
+    "net"
+    "os"
+)
+ 
+/* A Simple function to verify error */
+func CheckError(err error) {
+    if err  != nil {
+        fmt.Println("Error: " , err)
+        os.Exit(0)
+    }
+}
+ 
+func main() {
+    /* Lets prepare a address at any address at port 30000*/   
+    ServerAddr,err := net.ResolveUDPAddr("udp",":29999")
+    CheckError(err)
+ 
+    fmt.Println("ServerAddr: ", ServerAddr)
+
+    /* Now listen at selected port */
+    ServerConn, err := net.ListenUDP("udp", ServerAddr)
+    CheckError(err)
+    defer ServerConn.Close()
+ 
+    buf := make([]byte, 1024)
+ 
+    for {
+        n,addr,err := ServerConn.ReadFromUDP(buf)
+        fmt.Println("Received ",string(buf[0:n]), " from ",addr)
+ 
+        if err != nil {
+            fmt.Println("Error: ",err)
+        } 
+    }
+}
